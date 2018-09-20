1f672545dd58ac6f58394226cb0d92145d315bcf
ark234 <ark234@gmail.com>
2018-09-20 11:16:24 -0700

    adding script to load test the server

--- test.js
+++ test.js
@@ -1,2 +1,42 @@
 #!/usr/bin/env node
-console.log('Hellow, world!');
+const axios = require('axios');
+
+const config = {
+  requests: [
+    {
+      method: 'GET',
+      route: 'http://localhost:3333/slow',
+      requestNum: 2
+    },
+    {
+      method: 'GET',
+      route: 'http://localhost:3333/slower',
+      requestNum: 2
+    }
+  ]
+};
+
+// Helper method for pushing promise into array
+const promPush = (promArr, request) => {
+  if (request.method === 'GET') {
+    promArr.push(axios.get(request.route));
+  } else if (request.method === 'POST') {
+    // TODO
+  } else if (request.method === 'PUT') {
+    // TODO
+  } else if (request.method === 'DELETE') {
+    // TODO
+  } else if (request.method === 'PATCH') {
+    // TODO
+  } else {
+    return 'Invalid HTTP method!';
+  }
+};
+
+const promiseArr = [];
+config.requests.forEach(request => {
+  for (let i = 0; i < request.requestNum; i++) {
+    promPush(promiseArr, request);
+  }
+});
+axios.all(promiseArr).then(console.log('all requests have been processed!'));
