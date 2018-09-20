b33e4ea7e9796ba4c6d40dfc2b9bc3b664cf2cbf
Colin Roemer <colin.roemer@gmail.com>
2018-09-20 12:17:14 -0700

    building demo server

---
+++ test/demo-server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+
+
+const PORT = 3333;
+const app = expres();
+const extress = require('../extress');
+
+
+app.listen(PORT,
+  extress.map(app), 
+  console.log(`Listening on ${PORT}`)
+)
